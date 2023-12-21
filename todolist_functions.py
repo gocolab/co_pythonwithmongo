625e2405f1a5cf6daaa8e73fd4cdd8b424405970
youngji Jang <btg1631@naver.com>
2023-12-21 14:21:26 +0900

    modify

--- todolist_functions.py
+++ todolist_functions.py
@@ -19,6 +19,7 @@ def User_name(collection):
 
     result_participants = collection.insert_one({"user_name" : user_name})
     inserted_participants_id = result_participants.inserted_id
+    # 사용자 id를 return
     return inserted_participants_id
 
 def Todos(user_id, collection1, collection2):
@@ -33,17 +34,18 @@ def Todos(user_id, collection1, collection2):
         count+= 1
     print("")
 
-    #todo중 하나 입력
+    # todo중 하나 입력
     user_input = int(input("Title 번호: "))-1
     # Status 입력
     user_status = input("Status: ")
 
-    # 사용자 id와 사용자가 입력한 번호에 해당하는 title의 id를 저장
+    # 사용자가 입력한 번호에 해당하는 title과 그 title id를 찾음
     result_todo = collection1.find().skip(user_input).limit(1)
-    for doc in result_todo:
-        inserted_todo = doc['title']
-        inserted_todo_id = doc['_id']
-    # user_id, 사용자가 입력한 title과 그에 해당하는 title id, 사용자가 입력한 status를 collection2 컬렉션에 담기
+    for j in result_todo:
+        inserted_todo = j['title']
+        inserted_todo_id = j['_id']
+        
+    # user_id, 사용자가 입력한 title과 그 title id, 사용자가 입력한 status를 collection2에 담기
     collection2.insert_one({"user_id" : user_id, "user_todo_id" : inserted_todo_id, "todo_title" : inserted_todo, "user_status" : user_status})
 
 # 종료 여부 입력
